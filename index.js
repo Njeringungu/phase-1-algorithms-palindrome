68c94fffb4d36f459430018f5f7d3317c1a5a6dc
Janet Njeri Ngungu <janetnjerin89@gmail.com>
2022-06-16 22:49:22 +0300

    adding algorithms palindrome

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 function isPalindrome(word) {
   // Write your algorithm here
-}
+return word == word.split("").reverse().join("") ? true : false;
+  }
+  isPalindrome("abba") 
+ 
+
 
 /* 
   Add your pseudocode here
 */
-
+// reverse the input string 
+// If the string is same as reversed return true 
+// else return false
 /*
   Add written explanation of your solution here
 */
+//We use split to convert string into ann array.
+//We use reverse so as to reverse all the letters in that array.
+//we use join to convert the array back into a string.
+//use tenary method
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
